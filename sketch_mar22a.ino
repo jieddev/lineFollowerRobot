ae433a1087ccaaa94d8af1ed5a7d9128291fc098
DELJIE <jiedelflorentino.ruizo@gmail.com>
2025-03-25 21:29:33 +0800

    FIXED STALLING ISSUE

--- sketch_mar22a.ino
+++ sketch_mar22a.ino
@@ -26,7 +26,7 @@ int lastLeftState = WHITE;
 int lastRightState = WHITE;
 
 void normalSpeed() {
-  int speed = applySpeedConstraint(69);
+  int speed = applySpeedConstraint(70);
   analogWrite(ENA, speed);  // Set speed for Motor A (0-255)
   analogWrite(ENB, speed);  // Set speed for Motor B (0-255)
 }
